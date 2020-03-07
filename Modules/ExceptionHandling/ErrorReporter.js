3c69dd8c64eae78efb144444160366f21e8fd223
BernabePosadas <bernabe_posadas@yahoo.com>
2020-03-08 00:05:47 +0800

    remove attachment.
    changed to fetch image via github
    
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- Modules/ExceptionHandling/ErrorReporter.js
+++ Modules/ExceptionHandling/ErrorReporter.js
@@ -1,13 +1,11 @@
 const Discord = require("discord.js");
-const attachment = new Discord.Attachment(__dirname  + "/../../images/Hanako_Scraped_Knee(Exception).png", "Hanako_Scraped_Knee(Exception).png");
 exports.ReportErrorToDev = function (client, DevDiscordID, BotExceptionLogChannel, error) {
     
     client.channels.get(BotExceptionLogChannel).send(`<@${DevDiscordID}> whuaaahh! *sniff (T⌓T)`);
     var ExceptionBanner = new Discord.RichEmbed()
     .setColor("#FF0000")
     .setTitle("Hanako Encountered an Exception")
-    .attachFile(attachment)
-    .setImage("attachment://Hanako_Scraped_Knee(Exception).png")
+    .setImage("https://raw.githubusercontent.com/BernabePosadas/OkuhanaHanakov2/master/images/Hanako_Scraped_Knee(Exception).png")
     .addField("Hanako scrape her knee (Exception Thrown)", `\`\`\`${error.stack || error}\`\`\``, true);
     client.channels.get(BotExceptionLogChannel).send(ExceptionBanner);
 }
