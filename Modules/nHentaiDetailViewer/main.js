5911ae1256a3c966fd9ebd930dd7ec7712e6c90b
BernabePosadas <bernabe_posadas@yahoo.com>
2020-04-26 22:05:43 +0800

    nhentaiDoujinViewer : added more tag information display
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- Modules/nHentaiDetailViewer/main.js
+++ Modules/nHentaiDetailViewer/main.js
@@ -16,27 +16,25 @@ exports.nHentaiViewer = class {
             .setColor("#FFC0CB")
             .setTitle(doujinInfo.title.english)
             .setImage(`https://t.nhentai.net/galleries/${doujinInfo.media_id}/cover.${ext}`)
-            .addField("Tags: ", this.displayTags(doujinInfo.tags))
-            .addField("Artist: ", this.displayArtist(doujinInfo.tags))
+            .addField("Tags: ", this.displayTags(doujinInfo.tags, "tag"))
+            .addField("Artist: ", this.displayTags(doujinInfo.tags, "artist"))
+            .addField("Group: ", this.displayTags(doujinInfo.tags, "group"))
+            .addField("Character: ", this.displayTags(doujinInfo.tags, "character"))
+            .addField("Parody: ", this.displayTags(doujinInfo.tags, "parody"))
+            .addField("Language: ", this.displayTags(doujinInfo.tags, "language"))
             .addField(`${doujinInfo.images.pages.length} pages`, `[View on nHentai](https://nhentai.net/g/${doujinInfo.id})`, true);
         message.channel.send(DetailRichEmbed);
     }
-    displayTags(tags){
+    displayTags(tags, tagType){
         var tagList = "";
         tags.forEach(tag => {
-            if(tag.type == "tag"){
+            if(tag.type == tagType){
                 tagList += `[[${tag.name} (${tag.count})](https://nhentai.net/${tag.url})] `;
             }
         });
-        return tagList.trim();
-    }
-    displayArtist(tags){
-        var tagList = "";
-        tags.forEach(tag => {
-            if(tag.type == "artist"){
-                tagList += `[[${tag.name} (${tag.count})](https://nhentai.net/${tag.url})]`;
-            }
-        });
+        if(tagList === ""){
+            return "none";
+        }
         return tagList.trim();
     }
 }
