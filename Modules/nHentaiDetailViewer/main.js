cd567ec976e9c0bfef2eff9e163de2c28ae65664
BernabePosadas <bernabe_posadas@yahoo.com>
2020-04-26 21:54:52 +0800

    bug fixes on doujinviewer and error notifying not working
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- Modules/nHentaiDetailViewer/main.js
+++ Modules/nHentaiDetailViewer/main.js
@@ -16,8 +16,8 @@ exports.nHentaiViewer = class {
             .setColor("#FFC0CB")
             .setTitle(doujinInfo.title.english)
             .setImage(`https://t.nhentai.net/galleries/${doujinInfo.media_id}/cover.${ext}`)
-            .addField("Tags: ", this.displayTags(doujinInfo.tags), true)
-            .addField("Artist: ", this.displayArtist(doujinInfo.tags), true)
+            .addField("Tags: ", this.displayTags(doujinInfo.tags))
+            .addField("Artist: ", this.displayArtist(doujinInfo.tags))
             .addField(`${doujinInfo.images.pages.length} pages`, `[View on nHentai](https://nhentai.net/g/${doujinInfo.id})`, true);
         message.channel.send(DetailRichEmbed);
     }
@@ -25,16 +25,16 @@ exports.nHentaiViewer = class {
         var tagList = "";
         tags.forEach(tag => {
             if(tag.type == "tag"){
-                tagList += `[${tag.name} (${tag.count})](https://nhentai.net/${tag.url}) `;
+                tagList += `[[${tag.name} (${tag.count})](https://nhentai.net/${tag.url})] `;
             }
         });
         return tagList.trim();
     }
-    displayArtist(doujinInfo){
+    displayArtist(tags){
         var tagList = "";
         tags.forEach(tag => {
             if(tag.type == "artist"){
-                tagList += `[${tag.name} (${tag.count})](https://nhentai.net/${tag.url}) `;
+                tagList += `[[${tag.name} (${tag.count})](https://nhentai.net/${tag.url})]`;
             }
         });
         return tagList.trim();
