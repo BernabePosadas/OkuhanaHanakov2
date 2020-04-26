9a7ef0392f33bbe79120d82201a32f582eca5ea5
BernabePosadas <bernabe_posadas@yahoo.com>
2020-04-26 21:42:34 +0800

    nHentaiDoujinViewer : add tags and artist on doujin information;
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- Modules/nHentaiDetailViewer/main.js
+++ Modules/nHentaiDetailViewer/main.js
@@ -16,7 +16,27 @@ exports.nHentaiViewer = class {
             .setColor("#FFC0CB")
             .setTitle(doujinInfo.title.english)
             .setImage(`https://t.nhentai.net/galleries/${doujinInfo.media_id}/cover.${ext}`)
+            .addField("Tags: ", this.displayTags(doujinInfo.tags), true)
+            .addField("Artist: ", this.displayArtist(doujinInfo.tags), true)
             .addField(`${doujinInfo.images.pages.length} pages`, `[View on nHentai](https://nhentai.net/g/${doujinInfo.id})`, true);
         message.channel.send(DetailRichEmbed);
+    }
+    displayTags(tags){
+        var tagList = "";
+        tags.forEach(tag => {
+            if(tag.type == "tag"){
+                tagList += `[${tag.name} (${tag.count})](https://nhentai.net/${tag.url}) `;
+            }
+        });
+        return tagList.trim();
+    }
+    displayArtist(doujinInfo){
+        var tagList = "";
+        tags.forEach(tag => {
+            if(tag.type == "artist"){
+                tagList += `[${tag.name} (${tag.count})](https://nhentai.net/${tag.url}) `;
+            }
+        });
+        return tagList.trim();
     }
 }
