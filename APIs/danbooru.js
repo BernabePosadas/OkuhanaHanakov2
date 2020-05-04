0e743cfad15696276c5998596cf1dd2dc6362bf5
BernabePosadas <bernabe_posadas@yahoo.com>
2020-05-04 13:19:51 +0800

    changed kokkoro probability
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- APIs/danbooru.js
+++ APIs/danbooru.js
@@ -2,51 +2,47 @@ const fetch = require("node-fetch");
 const querystring = require("querystring");
 const ErrorChecker = require("./ErrorChecker");
 
-exports.fetchRandomImage_Arrows = async function(candidate){
+exports.fetchRandomImage_Arrows = async function(candidate) {
     const AuthHeader = process.env.USER_NAME + ":" + process.env.API_KEY;
     const query = querystring.stringify({
-        tags : candidate + " 1girl",
-        limit : "1",
-        page : "1",
-        random : "true"
+        tags: candidate + " 1girl",
+        limit: "1",
+        page: "1",
+        random: "true"
     });
     return await Request_GET_posts_danbooru(query, Buffer.from(AuthHeader).toString('base64'));
 }
 
-exports.fetchRandomImage = async function(searchQuery, searchFrom)
-{
+exports.fetchRandomImage = async function(searchQuery, searchFrom) {
     const AuthHeader = process.env.USER_NAME + ":" + process.env.API_KEY;
     const query = querystring.stringify({
-        tags : searchQuery,
-        limit : "1",
-        page : "1",
-        random : "true"
+        tags: searchQuery,
+        limit: "1",
+        page: "1",
+        random: "true"
     });
-    switch(searchFrom){
-        case "Safebooru" : 
-            return await Request_GET_posts_Safebooru(query,  Buffer.from(AuthHeader).toString('base64'));
-        default: 
-            return await Request_GET_posts_danbooru(query,  Buffer.from(AuthHeader).toString('base64'));
+    switch (searchFrom) {
+        case "Safebooru":
+            return await Request_GET_posts_Safebooru(query, Buffer.from(AuthHeader).toString('base64'));
+        default:
+            return await Request_GET_posts_danbooru(query, Buffer.from(AuthHeader).toString('base64'));
     }
 }
 
-async function Request_GET_posts_danbooru(query, header){
-    const response = await fetch(`https://danbooru.donmai.us/posts.json?${query}`, {headers : { Authorization : "Basic " + header}}).then(ErrorChecker.checkError).then((response) => response.json());
-    if(Object.keys(response).length){
+async function Request_GET_posts_danbooru(query, header) {
+    const response = await fetch(`https://danbooru.donmai.us/posts.json?${query}`, { headers: { Authorization: "Basic " + header } }).then(ErrorChecker.checkError).then((response) => response.json());
+    if (Object.keys(response).length) {
         return `https://danbooru.donmai.us/posts/${response[0].id}`;
-    }
-    else{
+    } else {
         return "no data";
     }
-   return "no data";
 }
 
-async function Request_GET_posts_Safebooru(query, header){
-    const response = await fetch(`https://safebooru.donmai.us/posts.json?${query}`, {headers : { Authorization : "Basic " + header}}).then(ErrorChecker.checkError).then((response) => response.json());
-    if(Object.keys(response).length){
+async function Request_GET_posts_Safebooru(query, header) {
+    const response = await fetch(`https://safebooru.donmai.us/posts.json?${query}`, { headers: { Authorization: "Basic " + header } }).then(ErrorChecker.checkError).then((response) => response.json());
+    if (Object.keys(response).length) {
         return `https://safebooru.donmai.us/posts/${response[0].id}`;
-    }
-    else{
+    } else {
         return "no data";
     }
 }
--- Modules/DanbooruImageRandomizer/ArrowPoolDeckGenerator.js
+++ Modules/DanbooruImageRandomizer/ArrowPoolDeckGenerator.js
@@ -1,4 +1,4 @@
-exports.createBernabeDeck = function () {
+exports.createBernabeDeck = function() {
     var candidates = [
         "futaba_anzu",
         "crescent_(azur_lane)",
@@ -11,32 +11,32 @@ exports.createBernabeDeck = function () {
     ];
     var probabilities = [
         112, // 56 % of 200 collections 
-        4,   // 2 %
-        4,   // 2 % 
-        4,   // 2 %
-        4 ,  // 2 %
-        4 ,  // 2 %
-        41,  // 20.5 %
-        27   // 13.5 %
+        4, // 2 %
+        4, // 2 % 
+        4, // 2 %
+        4, // 2 %
+        4, // 2 %
+        41, // 20.5 %
+        27 // 13.5 %
     ];
     return createDeck(candidates, probabilities);
-    
+
 }
 
 
-exports.createMarkDeck = function () {
+exports.createMarkDeck = function() {
     var candidates = [
         "abigail_williams_(fate/grand_order)",
         "kokkoro_(princess_connect!) "
     ];
     var probabilities = [
-        8,  // 80 % of 10 collections 
-        2   // 20%  
+        6, // 60 % of 10 collections 
+        4 // 40%  
     ];
     return createDeck(candidates, probabilities);
 }
 
-exports.createIvanDeck = function () {
+exports.createIvanDeck = function() {
     var candidates = [
         "lavender_(flower_knight_girl)",
         "hagi_(flower_knight_girl)",
@@ -44,23 +44,23 @@ exports.createIvanDeck = function () {
         "enokorogusa_(flower_knight_girl)",
         "jack_the_ripper_(fate/apocrypha)",
         "nursery_rhyme_(fate/extra)",
-        "megumin" 
+        "megumin"
     ];
     var probabilities = [
-        2,  // 10 % of 20 collections 
-        1,  // 5%
-        2,  // 10%
-        2,  // 10%
-        6,  // 30%
-        3,  // 15%
-        4,  // 20%
+        2, // 10 % of 20 collections 
+        1, // 5%
+        2, // 10%
+        2, // 10%
+        6, // 30%
+        3, // 15%
+        4, // 20%
     ];
     return createDeck(candidates, probabilities);
 }
 
-function createDeck(candidates, probabilities){
+function createDeck(candidates, probabilities) {
     var deck = [];
-    candidates.forEach(function (value, index) {
+    candidates.forEach(function(value, index) {
         var count = probabilities[index];
         while (count != 0) {
             deck.push(value);
