f675736996c294fbe72c46ef8d184f5f35d8a7d8
BernabePosadas <bernabe_posadas@yahoo.com>
2020-10-02 15:51:01 +0800

    created boilerplate css and js file
    Signed-off-by: BernabePosadas <bernabe_posadas@yahoo.com>

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Hanako } from "./bot";
 import exp from "express";
 import logger from "morgan";
 const app = exp();
+import path from "path";
 
 //region attributes
 const port = process.env.PORT || 3000;
@@ -15,16 +16,11 @@ bot.start().then(() => {
   console.log('<placeholder for start speech>');
   // Express.js Starts Here 
 
-  //region configs 
+  //region middleware
   app.use(logger("combined"));
-  app.use(exp.static('public'));
+  app.use('/OkuhanaHanako', exp.static(path.join(__dirname, 'static-pages')));  //serve static page
   //endregion
 
-  //region HTTP request route area 
-  app.get("/OkuhanaHanako/Home", (req, res) => {
-    //res.send("Hello world!");
-    return res.status(200).sendFile(__dirname + "/pages/main.htm");
-  })
   app.listen(port);
   //endregion
 
